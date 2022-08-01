11bdf4e84ae072f89e31b7b5567c7ff84589db39
Faan <rudfanno1@gmail.com>
2022-08-01 22:14:11 +0700

    closed portfolio submit

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
             <AboutMe />
             <Blog />
             <Contact />
-            <Portfolio/>
+            {/* <Portfolio/> */}
             </div>
           }>
           </Route>
